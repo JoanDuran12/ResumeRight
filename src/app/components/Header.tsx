52ef1dafef46f8ca57752db6bf2fc9d60b7e3df6
Joan Duran <joanmanuel808@gmail.com>
2025-03-28 00:10:14 -0400

    chore: increase z-index of header for improved visibility

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -51,7 +51,7 @@ function Header() {
   };
   
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex items-center justify-center">
+    <header className="sticky top-0 z-9999 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex items-center justify-center">
       <div className="container flex h-16 items-center justify-between px-24">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex items-center gap-2">
